a9bcc27ba8602d91d8ea6356ca8c5ac40fa821c4
Viktor Stoyanov <vikman.enterprise@gmail.com>
2022-09-05 14:22:42 +0200

    fastlane: moving plugin to another repository

--- fastlane-plugin-automated_versioning/lib/fastlane/plugin/automated_versioning/version.rb
+++
@@ -1,5 +0,0 @@
-module Fastlane
-  module AutomatedVersioning
-    VERSION = "0.1.0"
-  end
-end
--- fastlane-plugin-automated_versioning/spec/automated_versioning_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::AutomatedVersioningAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The automated_versioning plugin is working!")
-
-      Fastlane::Actions::AutomatedVersioningAction.run(nil)
-    end
-  end
-end
